b5fac252571dc7ce7febbb13c70ad9f6bd9046ad
Sourabh Aggarwal <devsourabhagg@gmail.com>
2025-04-26 18:05:16 +0530

    testing performance

--- src/test/java/org/example/ArraysCompareTest.java
+++ src/test/java/org/example/ArraysCompareTest.java
@@ -36,4 +36,15 @@ public class ArraysCompareTest {
         int[] numbers = null;
             Arrays.sort(numbers);
     }
+
+    @Test(timeout = 100)
+    public void testSort_Performance(){
+        int array[] = {12,33,4};
+        for(int i=1;i<1000000;i++){
+            array[0] = i;
+            Arrays.sort(array);
+        }
+
+    }
+
 }
